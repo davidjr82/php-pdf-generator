e883708f2c7e3f692950f351acc5d7d58c326522
David Jiménez <djimenez@edatta.com>
2020-06-28 09:50:25 +0200

    wip

--- src/Exceptions/PDFGeneratorException.php
+++ src/Exceptions/PDFGeneratorException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class PDFGeneratorException extends Exception
 {
-    public function __construct($message, $add_install_suggestion = false)
+    public function __construct(string $message, bool $add_install_suggestion = false)
     {
         if ($add_install_suggestion) {
             $message .= PHP_EOL . 'Suggestion: Install full latex requirements: apt-get update && apt-get install texlive-full poppler-utils';
--- tests/PDFGeneratorTest.php
+++ tests/PDFGeneratorTest.php
@@ -4,6 +4,7 @@ namespace Davidjr82\PhpPDFGenerator\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Davidjr82\PhpPDFGenerator\PDFGenerator;
+use Davidjr82\PhpPDFGenerator\Exceptions\PDFGeneratorException;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class PDFGeneratorTest extends TestCase
@@ -12,5 +13,11 @@ class PDFGeneratorTest extends TestCase
     {
         $this->expectException(ProcessFailedException::class);
         $generator = new PDFGenerator();
+    }
+
+    public function test_default_exception()
+    {
+        $exception = new PDFGeneratorException("Exception message");
+        $this->assertEquals("Exception message", $exception->getMessage());
     }
 }
